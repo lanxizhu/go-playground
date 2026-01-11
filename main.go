33f40c4c53cb0c22ad9a8c3470e29e0a6a6d4232
Lance Choo <2332778528@qq.com>
2026-01-11 20:11:50 +0800

    feat: Add basic Gin web server

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+func main() {
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	err := router.Run()
+	if err != nil {
+		return
+	} // listens on 0.0.0.0:8080 by default
+}
