c5736a47c960c29e2a16856482d9bd43911cc739
Lance Choo <2332778528@qq.com>
2026-01-12 21:45:45 +0800

    feat: Load environment variables

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
 import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	// _ "github.com/joho/godotenv/autoload"
 	"github.com/lanxizhu/go-playground/router"
 )
 
 func main() {
+	if err := godotenv.Load(".env"); err != nil {
+		log.Println("No .env file found, using default environment variables")
+	} else {
+		log.Println(".env file loaded successfully")
+	}
+
+	port := os.Getenv("PORT")
+	log.Println("PORT: " + port)
+
 	r := router.SetupRouter()
 
 	err := r.Run()
